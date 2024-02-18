8dd5e85faeaba1d7a1c224d2188523032e83d0e8
Angrypig123 <shguddnr3@naver.com>
2024-02-18 23:05:21 +0900

    initializer

---
+++ src/test/java/org/spring/example/jpa/JpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.example.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
