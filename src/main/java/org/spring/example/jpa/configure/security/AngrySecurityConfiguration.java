06df5a6a073f0d442cb2a8440b446ab5a7484ae2
Angrypig123 <shguddnr3@naver.com>
2024-02-23 17:52:41 +0900

    .

--- src/main/java/org/spring/example/jpa/configure/security/AngrySecurityConfiguration.java
+++ src/main/java/org/spring/example/jpa/configure/security/AngrySecurityConfiguration.java
@@ -2,11 +2,14 @@ package org.spring.example.jpa.configure.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.spring.example.jpa.configure.security.filter.CsrfCookieFilter;
 import org.spring.example.jpa.configure.security.filter.CsrfTokenLoggerFilter;
 import org.spring.example.jpa.configure.security.filter.CsrfTokenValidFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,17 +28,12 @@ public class AngrySecurityConfiguration {
     private final CsrfTokenLoggerFilter csrfTokenLoggerFilter;
     private final CsrfTokenValidFilter csrfTokenValidFilter;
 
+    @Value("${spring.profiles.active}")
+    private String ACTIVE;
+
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-
-        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
-        requestHandler.setCsrfRequestAttributeName("_csrf");
-
-        http
-                .addFilterAfter(csrfTokenLoggerFilter, BasicAuthenticationFilter.class) //
-                .addFilterAfter(csrfTokenValidFilter, BasicAuthenticationFilter.class)
-                .cors((cors) -> cors.configurationSource(customCorsConfig))
-                .csrf((csrf) -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+        httpSecurity(http)
                 .authorizeHttpRequests(
                         (requests) -> requests
                                 .requestMatchers("/basic").permitAll()
@@ -46,6 +44,26 @@ public class AngrySecurityConfiguration {
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults());
         return http.build();
+    }
+
+    private HttpSecurity httpSecurity(HttpSecurity http) throws Exception {
+        if (ACTIVE.equals("dev")) {
+            return http
+                    .cors(AbstractHttpConfigurer::disable)
+                    .csrf(AbstractHttpConfigurer::disable);
+        } else {
+            CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+            requestHandler.setCsrfRequestAttributeName("_csrf");
+            return http
+                    .addFilterAfter(csrfTokenLoggerFilter, BasicAuthenticationFilter.class)
+                    .addFilterAfter(csrfTokenValidFilter, BasicAuthenticationFilter.class)
+                    .cors((cors) -> cors.configurationSource(customCorsConfig))
+                    .csrf((csrf) -> csrf
+                            .csrfTokenRequestHandler(requestHandler)
+                            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    )
+                    .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
+        }
     }
 
 
