e8a540bc163c2ebb9c92b4f92b334d4731e355af
Angrypig123 <shguddnr3@naver.com>
2024-02-24 20:01:17 +0900

    package modify

--- src/main/java/org/spring/example/jpa/configure/controlleradvice/GlobalControllerAdvice.java
+++ src/main/java/org/spring/example/jpa/configure/controlleradvice/GlobalControllerAdvice.java
@@ -1,4 +1,4 @@
-package org.spring.example.jpa.configure;
+package org.spring.example.jpa.configure.controlleradvice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.spring.example.jpa.util.exception.CustomArgumentNotValidException;
